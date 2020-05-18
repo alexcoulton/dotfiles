fbf5c454077acf9529401bc3f81d48f2b8da264c
Alex Coulton <alexcoulton@gmail.com>
2020-05-18 11:27:54 +0100

    updated vimrc / tmux conf

--- pushvim.sh
+++ pushvim.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cp ~/.vimrc ~/dotfiles/
-cp ~/.tmux.conf ~/vimrc
+cp ~/.tmux.conf ~/dotfiles
 cp ~/.vimnotes ~/dotfiles/
-cd ~/vimrc
-git add -A ~/dotfiles/.vimrc
-git add -A ~/dotfiles/.vimnotes
-git add -A ~/dotfiles/.tmux.conf
+cd ~/dotfiles
+#git add -A ~/dotfiles/.vimrc
+git add -A ./*
+#git add -A ~/dotfiles/.vimnotes
+#git add -A ~/dotfiles/.tmux.conf
 git commit -m 'updated vimrc / tmux conf'
 git push origin master
 
