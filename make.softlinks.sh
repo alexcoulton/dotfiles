78a5e8ed8f7a28f125ff3e5b18b15062d9c1068c
Alex Coulton <alexcoulton@gmail.com>
2021-08-09 10:21:04 +0100

    update dotfiles

---
+++ make.softlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.ctags ~/.ctags
+
+
+
--- pullvim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git pull origin master
-
-cp .vimrc ~/
-cp .tmux.conf ~/
-cp .vimnotes ~/
-
-
